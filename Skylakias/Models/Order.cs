8b103b4035e29144214b7d0c0642a9e54574f480
apinis <amfipin@gmail.com>
2022-01-12 12:25:33 +0200

    Add project files.

---
+++ Skylakias/Models/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Skylakias.Models
+{
+    public class Order
+    {
+
+        [Key, Column(Order = 0)]
+        [ForeignKey("ApplicationUser")]
+        public string ApplicationUserId { get; set; }
+
+        [Key, Column(Order = 1)]
+        public int ServiceId { get; set; }
+        public int TotalPrice { get; set; }
+        public virtual Service Service { get; set; }
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+    }
+}
---
+++ Skylakias/Models/Service.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Skylakias.Models
+{
+    public class Service
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [StringLength(255)]
+        public string Description { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
